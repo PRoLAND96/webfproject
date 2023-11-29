ece249aca25a143d5bfcef7c5bb13df140e4f831
Pető Roland <petoroli2001@gmail.com>
2023-11-29 13:38:15 +0100

    created WebfprojectApplication.java and ServletInitializer.java files

---
+++ src/main/java/hu/project/webfproject/WebfprojectApplication.java
@@ -0,0 +1,15 @@
+package hu.project.webfproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class WebfprojectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebfprojectApplication.class, args);
+    }
+
+}
+
